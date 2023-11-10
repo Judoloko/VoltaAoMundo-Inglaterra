0b49963285783f8d13e7befad5b89f1ebafc4832
Judoloko <judoloko.85@gmail.com>
2023-11-10 06:07:00 -0300

    php do testLogin

---
+++ src/php/testLogin.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+  // print_r($_REQUEST)  
+  if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
+  {
+    // Acessa
+    include_once('config.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // echo ('User: ' . $user);
+    // echo ('<br>');
+    // echo ('Senha: ' . $senha);
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'"; 
+
+    $resultado = $conexao->query($sql);
+
+    // print_r($sql);
+    // print_r($resultado);
+  
+    if(mysqli_num_rows($resultado) < 1) {
+      unset($_SESSION['email']);
+      unset($_SESSION['senha']);
+      header('Location: login.php');
+    } else {
+      $_SESSION['email'] = $email;
+      $_SESSION['senha'] = $senha;
+      header('Location: sistema.php ');
+    }
+
+
+  } else {
+
+    // Não acessa
+    header('Location: login.php');    
+  }
+?>
