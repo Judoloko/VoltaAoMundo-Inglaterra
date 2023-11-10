4a3a8ed204352541ed0652239cf91681f170b906
Judoloko <judoloko.85@gmail.com>
2023-11-10 06:03:58 -0300

    php do home

---
+++ src/php/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <!-- Meta tags Obrigatórias -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="../assets/inglaterra.png" type="image/png">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+
+
+  <!-- Link para meu CSS -->
+  <link rel="stylesheet" href="../css/home.php.css">
+
+ 
+
+  <title>Volta Ao Mundo - Inglaterra</title>
+</head>
+<body>
+  <br><br>
+  <h1>Bem-vindo ao Nosso Sistema!</h1>
+  <h2>Escolha a opção abaixo para fazer login ou se tornar um Administrador:</h2>
+  <div class="box">
+    <a href="login.php">Login</a>
+    <a href="formulario.php">Cadastra-se</a>
+  </div>
+</body>
+</html>
